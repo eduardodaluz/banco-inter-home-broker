6eed59da2698f6eb75506608a1d43594fd8d618c
Eduardo Rodrigues da Luz <eduardo.r.luz@gmail.com>
2019-03-11 08:00:17 -0300

    Create banco-inter-home-broker.user.js

---
+++ banco-inter-home-broker.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         Banco Inter Home Broker
+// @namespace    http://devdevdev.dev/
+// @version      0.1
+// @description  Prevents Inter Bank's Home Broker to close session.
+// @author       Carimbador
+// @match        https://home-broker.bancointer.com.br/hbnet2/hbweb2/Default.aspx
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(function() {
+        var mouseMoveEvent = document.createEvent("MouseEvents");
+
+        mouseMoveEvent.initMouseEvent(
+            "mousemove", //event type : click, mousedown, mouseup, mouseover, mousemove, mouseout.
+            true, //canBubble
+            false, //cancelable
+            window, //event's AbstractView : should be window
+            1, // detail : Event's mouse click count
+            50, // screenX
+            50, // screenY
+            50, // clientX
+            50, // clientY
+            false, // ctrlKey
+            false, // altKey
+            false, // shiftKey
+            false, // metaKey
+            0, // button : 0 = click, 1 = middle button, 2 = right button
+            null // relatedTarget : Only used with some event types (e.g. mouseover and mouseout). In other cases, pass null.
+        );
+
+        document.dispatchEvent(mouseMoveEvent)
+    }, 10000)
+})();
